0d63319a13b920740a9ccd620690926d66be56b0
Sam <git@samueldgv.com>
2022-03-26 21:16:46 +0100

    Representación del grafo de adyacencia de Risk
    Por Guillermo y Samuel

--- src/main.go
+++ src/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"backend/globales"
 	"backend/servidor"
 )
 
 func main() {
+	globales.InicializarGrafoMapa()
+
 	servidor.IniciarServidor(false)
 }
--- src/vo/vo.go
+++ src/vo/vo.go
@@ -23,48 +23,48 @@ const (
 )
 
 const (
-	eastern_australia NumRegion = iota
-	indonesia
-	new_guinea
-	alaska
-	ontario
-	northwest_territory
-	venezuela
-	madagascar
-	north_africa
-	greenland
-	iceland
-	great_britain
-	scandinavia
-	japan
-	yakursk
-	kamchatka
-	siberia
-	ural
-	afghanistan
-	middle_east
-	india
-	siam
-	china
-	mongolia
-	irkutsk
-	ukraine
-	southern_europe
-	western_europe
-	northern_europe
-	egypt
-	east_africa
-	congo
-	south_africa
-	brazil
-	argentina
-	eastern_united_states
-	western_united_states
-	quebec
-	central_america
-	peru
-	western_australia
-	alberta
+	Eastern_australia NumRegion = iota
+	Indonesia
+	New_guinea
+	Alaska
+	Ontario
+	Northwest_territory
+	Venezuela
+	Madagascar
+	North_africa
+	Greenland
+	Iceland
+	Great_britain
+	Scandinavia
+	Japan
+	Yakursk
+	Kamchatka
+	Siberia
+	Ural
+	Afghanistan
+	Middle_east
+	India
+	Siam
+	China
+	Mongolia
+	Irkutsk
+	Ukraine
+	Southern_europe
+	Western_europe
+	Northern_europe
+	Egypt
+	East_africa
+	Congo
+	South_africa
+	Brazil
+	Argentina
+	Eastern_united_states
+	Western_united_states
+	Quebec
+	Central_america
+	Peru
+	Western_australia
+	Alberta
 )
 
 // Usuario es un objeto de usuario equivalente al del modelo de base de datos.
@@ -224,7 +224,7 @@ func (p *Partida) InicializarAcciones() {
 
 func crearEstadoMapa() (mapa map[NumRegion]EstadoRegion) {
 	mapa = make(map[NumRegion]EstadoRegion)
-	for i := eastern_australia; i <= alberta; i++ {
+	for i := Eastern_australia; i <= Alberta; i++ {
 		mapa[i] = EstadoRegion{ocupante: "", numTropas: 0}
 	}
 
@@ -233,7 +233,7 @@ func crearEstadoMapa() (mapa map[NumRegion]EstadoRegion) {
 
 func crearBaraja() (cartas []Carta) {
 	numTiposTropa := 0
-	for i := eastern_australia; i <= alberta; i++ {
+	for i := Eastern_australia; i <= Alberta; i++ {
 		var carta Carta
 		var tipo TipoTropa
 		if numTiposTropa < 18 {
