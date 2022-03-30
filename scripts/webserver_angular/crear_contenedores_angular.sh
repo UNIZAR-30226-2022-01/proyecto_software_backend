568fd6121cf69f3804bf22f89c3b2f378cf4b822
Sam <git@samueldgv.com>
2022-03-30 13:49:53 +0200

    Mover /src a su carpeta padre para facilitar el indexado por godoc, con el código y go.mod al mismo nivel que la raíz del proyecto en github

--- scripts/webserver_angular/crear_contenedores_angular.sh
+++ scripts/webserver_angular/crear_contenedores_angular.sh
@@ -9,7 +9,7 @@ cp -r ./dist/angular  ../../proyecto_software_backend/build/angular/web
 
 
 # Compila el backend sin depender de librerías de C y trae el ejecutable a la carpeta local
-cd ../../proyecto_software_backend/src && CGO_ENABLED=0 go build -o backend main.go && mv backend ../build/angular && cp servidor.env ../build/angular &&cd ../build/angular
+cd ../../proyecto_software_backend && CGO_ENABLED=0 go build -o backend main.go && mv backend ./build/angular && cp servidor.env ./build/angular && cd ./build/angular
 
 sudo docker-compose up --detach
 
--- test/webserver-pruebas/globales/globales.go
+++
@@ -1,9 +0,0 @@
-// Package globales contiene variables globales a ser utilizadas por todos los módulos, instanciadas
-// desde el paquete principal
-package globales
-
-import (
-	"database/sql" // Funciones de sql
-)
-
-var Db *sql.DB // Base de datos thread safe, a compartir entre los módulos
