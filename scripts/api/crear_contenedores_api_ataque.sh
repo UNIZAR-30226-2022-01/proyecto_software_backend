7f179da64a4285f12ca4e680a2d12d69c2eab934
Sam <git@samueldgv.com>
2022-04-24 11:21:55 +0200

    Scripts de poblado de base de datos con partidas de prueba

---
+++ scripts/api/crear_contenedores_api_ataque.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Compila el backend sin depender de librerías de C y trae el ejecutable a la carpeta local
+cd ../../ && CGO_ENABLED=0 go build -o backend main.go && mv backend ./build/api && cp servidor.env ./build/api && cd ./build/api
+
+# Crea el volúmen de la BD
+sudo docker volume create volumen-postgres
+
+# Descomenta línea de DDL de ataque
+sed -i '/ataque/s/^#//g' docker-compose.yml
+
+# Crea los contenedores
+sudo docker-compose up --detach
+
+# Comenta línea de DDL de ataque
+sed -i '/ataque/s/^/#/g' docker-compose.yml
+
+rm ./backend
